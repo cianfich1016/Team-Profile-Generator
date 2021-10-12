40c72d13c80119202149c55932de386b36afb463
Rachel Cianfichi <cianfich@gmail.com>
2021-10-12 16:21:43 -0400

    Completed Intern test and tested all for functionality.

---
+++ test/Intern.test.js
@@ -0,0 +1,32 @@
+const Intern = require ("../lib/Intern");
+
+describe ("Intern", () => {
+    describe("constructor", () =>{
+        it("should create an object as an extension of the 'Employee' class and set 3 parameters of 'name', 'id', and 'email' in that order as well as a 'school' parameter", () => {
+            const name = "Melissa";
+            const id = 4321;
+            const email = "uncmelissa@gmail.com";
+            const school = "UNC Chapel Hill"
+            const testIntern = new Intern (name, id, email, school);
+            expect(testIntern.name).toBe(name);
+            expect(testIntern.id).toBe(id);
+            expect(testIntern.email).toBe(email);
+            expect(testIntern.school).toBe(school);
+        });
+    });
+
+    describe("getSchool()", () => {
+        it("should return the value of what was stored in the 'officeNumber' parameter", () => {
+            const school = "UNC Chapel Hill"
+            const testIntern = new Intern ("Melissa", 4321, "uncmelissa@gmail.com", school)
+            expect(testIntern.getSchool()).toBe(school)
+        });
+    });
+    describe("getRole()", () => {
+        it("should return the value 'Intern' which was returned from the function", () => {
+            const role = "Intern"
+            const testIntern = new Intern ("Melissa", 4321, "uncmelissa@gmail.com", "UNC Chapel Hill")
+            expect(testIntern.getRole()).toBe(role)
+        });
+    });
+});
